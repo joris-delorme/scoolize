c889b35ea742f3ae9584a7e46d8959556a67be91
joris-delorme <joris.delorme@epitech.digital>
2023-12-30 11:41:17 +0100

    missing push

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -13,32 +13,6 @@ export const { auth, signIn, signOut } = NextAuth({
     providers: [
         Credentials({
             async authorize(credentials) {
-
-                console.log(credentials);
-                
-                const parsedCredentials = z
-                    .object({ email: z.string().email(), password: z.string().min(6) })
-                    .safeParse(credentials);
-
-                console.log("parsedCredentials", parsedCredentials.success);
-                
-
-                if (parsedCredentials.success) {
-                    const { email, password } = parsedCredentials.data;
-
-                    const user = await db.query.users.findFirst({
-                        where: eq(users.email, email)
-                    })
-
-                    if (!user) return null
-                    console.log("user.password", user.password);
-                    if(!user.password) return user
-                    const passwordsMatch = await bcryptjs.compare(password, user.password)
-
-                    console.log("passwordsMatch", passwordsMatch);
-                    
-                    if (passwordsMatch) return user
-                }
                 return null;
             },
         }),
